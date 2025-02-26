979cd19040d07a4df82cbe3f1af3105e832a80e6
sahilsam05 <153838061+sahilsam05@users.noreply.github.com>
2025-02-26 13:45:12 +0000

    displaying and attempting to read csv

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iomanip>
 #include <iostream>
+#include <vector>
+#include <fstream>
 
 using namespace std;
 
@@ -16,14 +18,52 @@ struct Stocks
     float returnOnInvestment;
 };
 
-void display(const Stocks& stocks)
+void displayStocks(const vector<Stocks>& stockList)
 {
-    cout << left << setw(15) << stocks.name;
-    cout << setw(5) << stocks.category;
-    cout << setw(10) << stocks.yearFounded;
-    cout << setw(50) << stocks.moneyInvested;
-    cout << setw(30) << stocks.returnOnInvestment;
-    cout << endl;
+    cout << left << setw(25) << "Stock Name"
+         << setw(15) << "Category"
+         << setw(15) << "Year Founded"
+         << setw(20) << "Money Invested"
+         << setw(15) << "ROI" << endl;
+    cout << string(90, '-') << endl;
+}
+
+void parseLine(string line,Stocks &stocks) {
+    stringstream ss(line);
+    getline(ss, stocks.name, ',');
+    getline(ss, stocks.category, ',');
+    stocks.yearFounded = stoi(stocks.name);
+    stocks.moneyInvested = stoi(stocks.category);
+    stocks.returnOnInvestment = stoi(stocks.name);
+
+}
+
+//https://github.com/delboy8080/FileIO_Struct_SD2a/blob/master/main.cpp
+
+void load(string filename, vector<Stocks> &data)
+{
+    ifstream file(filename);
+
+    if (file)
+        {
+         string line;
+        while (getline(file, line))
+            {
+            Stocks stock;
+            parseLine(line, stock);
+            data.push_back(stock);
+        }
+        file.close();
+    }
+    else
+    {
+        cout << "Unable to open file" << endl;
+    }
+}
+
+void writeToFile(const vector<Stocks> &data)
+{
+
 }
 
 
@@ -31,6 +71,5 @@ void display(const Stocks& stocks)
 
 int main()
 {
-    s
     }
 
